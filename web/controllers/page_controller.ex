8ef810d15f78b0692aa663ab5e5ddcf2f335f159
Rabea Gleissner <rabeagleissner@gmail.com>
2016-05-23 13:55:18 +0100

    updates sessions with correct game state

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,7 +2,9 @@ defmodule TicTacToe.PageController do
   use TicTacToe.Web, :controller
 
   def index(conn, _params) do
-    conn = put_session(conn, :board, Board.empty_board)
+    if get_session(conn, :board) == nil do
+      conn = put_session(conn, :board, Board.empty_board)
+    end
     conn
     |> assign(:board, Board.empty_board)
     |> render("index.html")
@@ -10,6 +12,7 @@ defmodule TicTacToe.PageController do
 
   def move(conn, params) do
     {_, board} = Board.place_mark(get_session(conn, :board), users_move(params))
+    conn = put_session(conn, :board, board)
     conn
     |> assign(:board, board)
     |> render("index.html")
