72345b8b45745bc589cd33a523290647b3d6749e
Rabea Gleissner <rabeagleissner@gmail.com>
2016-05-23 17:43:07 +0100

    extracts methods for update params

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,17 +1,9 @@
 defmodule TicTacToe.PageController do
   use TicTacToe.Web, :controller
 
-  def index(conn, _params) do
-    if get_session(conn, :board) == nil do
-      conn = put_session(conn, :board, Board.empty_board)
-      conn = put_session(conn, :game_over, false)
-    end
-
-    if Board.game_over?(get_session(conn, :board)) do
-      conn = put_session(conn, :game_over, true)
-    else
-      conn = put_session(conn, :game_over, false)
-    end
+  def index(conn, _) do
+    conn = update_session(conn)
+    conn = clear_session_on_game_over(conn)
 
     conn
     |> assign(:board, get_session(conn, :board))
@@ -28,7 +20,7 @@ defmodule TicTacToe.PageController do
     redirect conn, to: "/"
   end
 
-  def replay(conn, params) do
+  def replay(conn, _) do
       conn = put_session(conn, :board, Board.empty_board)
     |> assign(:board, Board.empty_board)
     |> assign(:game_over, get_session(conn, :game_over))
@@ -39,5 +31,22 @@ defmodule TicTacToe.PageController do
     {number, _} = params["move"]
                   |> Integer.parse
     number
+  end
+
+  defp clear_session_on_game_over(conn) do
+    if Board.game_over?(get_session(conn, :board)) do
+      conn = put_session(conn, :game_over, true)
+    else
+      conn = put_session(conn, :game_over, false)
+    end
+    conn
+  end
+
+  defp update_session(conn) do
+    if get_session(conn, :board) == nil do
+      conn = put_session(conn, :board, Board.empty_board)
+      conn = put_session(conn, :game_over, false)
+    end
+    conn
   end
 end
