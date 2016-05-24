c4594a1c25f380008a329364265289d29333b512
Rabea Gleissner <rabeagleissner@gmail.com>
2016-05-24 15:04:40 +0100

    uses constant for empty board

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -1,14 +1,16 @@
 defmodule TicTacToe.PageControllerTest do
   use TicTacToe.ConnCase
 
+  @empty_board "123456789"
+
   test "get request to root redirects user to a new game", %{conn: conn} do
     conn = get conn, "/"
     assert html_response(conn, 302)
-    assert conn.resp_body =~ "You are being <a href=\"/game/123456789\">redirected</a>"
+    assert conn.resp_body =~ "You are being <a href=\"/game/#{@empty_board}\">redirected</a>"
   end
 
   test "get request to /game/123456789 shows an empty game board", %{conn: conn} do
-    conn = get conn, "/game/123456789"
+    conn = get conn, "/game/#{@empty_board}"
     assert conn.state == :sent
     assert conn.status == 200
     assert conn.resp_body =~ link_to_move(1)
@@ -23,12 +25,12 @@ defmodule TicTacToe.PageControllerTest do
   end
 
   test "get request to /game/123456789/4 makes human and computer move on the board", %{conn: conn} do
-    conn = get conn, "/game/123456789/4"
+    conn = get conn, "/game/#{@empty_board}/4"
     assert conn.resp_body =~ "You are being <a href=\"/game/O23X56789\">redirected</a>"
   end
 
   test "get request to /game/123456789/4 redirects the user", %{conn: conn} do
-    conn = get conn, "/game/123456789/4"
+    conn = get conn, "/game/#{@empty_board}/4"
     assert html_response(conn, 302)
   end
 
@@ -40,10 +42,10 @@ defmodule TicTacToe.PageControllerTest do
   test "get request for /replay redirects user to new game", %{conn: conn} do
     conn = get conn, "/replay"
     assert html_response(conn, 302)
-    assert conn.resp_body =~ "You are being <a href=\"/game/123456789\">redirected</a>"
+    assert conn.resp_body =~ "You are being <a href=\"/game/#{@empty_board}\">redirected</a>"
   end
 
   defp link_to_move(move) do
-    "\"123456789/#{move}\">&nbsp;</a>"
+    "\"#{@empty_board}/#{move}\">&nbsp;</a>"
   end
 end
